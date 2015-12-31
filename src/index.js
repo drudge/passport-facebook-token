f4627fb6ee9cc0ae94ef1cb86272db818cf0515b
Jonathan <fontajo@yahoo.com>
2015-12-31 13:57:58 -0500

    updated to simplify lookup method and parsing based on ES6 syntax

--- src/index.js
+++ src/index.js
@@ -24,173 +24,179 @@ import { OAuth2Strategy, InternalOAuthError } from 'passport-oauth';
  * });
  */
 export default class FacebookTokenStrategy extends OAuth2Strategy {
-  constructor(_options, _verify) {
-    let options = _options || {};
-    let verify = _verify;
+    constructor(_options, _verify) {
+        let options = _options || {};
+        let verify = _verify;
 
-    options.authorizationURL = options.authorizationURL || 'https://www.facebook.com/v2.4/dialog/oauth';
-    options.tokenURL = options.tokenURL || 'https://graph.facebook.com/oauth/access_token';
+        options.authorizationURL = options.authorizationURL || 'https://www.facebook.com/v2.4/dialog/oauth';
+        options.tokenURL = options.tokenURL || 'https://graph.facebook.com/oauth/access_token';
 
-    super(options, verify);
+        super(options, verify);
 
-    this.name = 'facebook-token';
-    this._authorizationField = options.authorizationField || 'Authorization';
-    this._accessTokenField = options.accessTokenField || 'access_token';
-    this._refreshTokenField = options.refreshTokenField || 'refresh_token';
-    this._profileURL = options.profileURL || 'https://graph.facebook.com/v2.4/me';
-    this._profileFields = options.profileFields || ['id', 'displayName', 'name', 'emails'];
-    this._clientSecret = options.clientSecret;
-    this._enableProof = typeof options.enableProof === 'boolean' ? options.enableProof : true;
-    this._passReqToCallback = options.passReqToCallback;    
-    this._oauth2.useAuthorizationHeaderforGET(false);
-  }
-
-  /**
-   * Authenticate request by delegating to a service provider using OAuth 2.0.
-   * @param {Object} req
-   * @param {Object} options
-   */
-  authenticate(req, options) {
-    let accessToken =  (FacebookTokenStrategy.parseAccessToken(req, this._accessTokenField, this._authorizationField));
-    let refreshToken = (req.body && req.body[this._refreshTokenField]) || (req.query && req.query[this._refreshTokenField]) || (req.headers && req.headers[this._refreshTokenField]);
-    
-    if (!accessToken) return this.fail({message: `You should provide ${this._accessTokenField}`});
-
-    this._loadUserProfile(accessToken, (error, profile) => {
-      if (error) return this.error(error);
-
-      const verified = (error, user, info) => {
-        if (error) return this.error(error);
-        if (!user) return this.fail(info);
-
-        return this.success(user, info);
-      };
-
-      if (this._passReqToCallback) {
-        this._verify(req, accessToken, refreshToken, profile, verified);
-      } else {
-        this._verify(accessToken, refreshToken, profile, verified);
-      }
-    });
-  }
-
-  /**
-   * Retrieve user profile from Facebook.
-   *
-   * This function constructs a normalized profile, with the following properties:
-   *
-   *   - `provider`         always set to `facebook`
-   *   - `id`               the user's Facebook ID
-   *   - `username`         the user's Facebook username
-   *   - `displayName`      the user's full name
-   *   - `name.familyName`  the user's last name
-   *   - `name.givenName`   the user's first name
-   *   - `name.middleName`  the user's middle name
-   *   - `gender`           the user's gender: `male` or `female`
-   *   - `profileUrl`       the URL of the profile for the user on Facebook
-   *   - `emails`           the proxied or contact email address granted by the user
-   *
-   * @param {String} accessToken
-   * @param {Function} done
-   */
-  userProfile(accessToken, done) {
-    let url = uri.parse(this._profileURL);
-
-    if (this._enableProof) {
-      // For further details, refer to https://developers.facebook.com/docs/reference/api/securing-graph-api/
-      let proof = crypto.createHmac('sha256', this._clientSecret).update(accessToken).digest('hex');
-      url.search = `${url.search ? url.search + '&' : ''}appsecret_proof=${encodeURIComponent(proof)}`;
+        this.name = 'facebook-token';
+        this._accessTokenField = options.accessTokenField || 'access_token';
+        this._refreshTokenField = options.refreshTokenField || 'refresh_token';
+        this._profileURL = options.profileURL || 'https://graph.facebook.com/v2.4/me';
+        this._profileFields = options.profileFields || ['id', 'displayName', 'name', 'emails'];
+        this._clientSecret = options.clientSecret;
+        this._enableProof = typeof options.enableProof === 'boolean' ? options.enableProof : true;
+        this._passReqToCallback = options.passReqToCallback;
+        this._oauth2.useAuthorizationHeaderforGET(false);
     }
 
-    if (this._profileFields) {
-      let fields = FacebookTokenStrategy.convertProfileFields(this._profileFields);
-      url.search = `${url.search ? url.search + '&' : ''}fields=${fields}`;
+    /**
+     * Authenticate request by delegating to a service provider using OAuth 2.0.
+     * @param {Object} req
+     * @param {Object} options
+     */
+    authenticate(req, options) {
+        let accessToken = this.lookup(req, this._accessTokenField);
+        let refreshToken = this.lookup(req, this._refreshTokenField);
+
+        if (!accessToken) return this.fail({ message: `You should provide ${this._accessTokenField}` });
+
+        this._loadUserProfile(accessToken, (error, profile) => {
+            if (error) return this.error(error);
+
+            const verified = (error, user, info) => {
+                if (error) return this.error(error);
+                if (!user) return this.fail(info);
+
+                return this.success(user, info);
+            };
+
+            if (this._passReqToCallback) {
+                this._verify(req, accessToken, refreshToken, profile, verified);
+            } else {
+                this._verify(accessToken, refreshToken, profile, verified);
+            }
+        });
     }
 
-    url = uri.format(url);
+    /**
+     * Retrieve user profile from Facebook.
+     *
+     * This function constructs a normalized profile, with the following properties:
+     *
+     *   - `provider`         always set to `facebook`
+     *   - `id`               the user's Facebook ID
+     *   - `username`         the user's Facebook username
+     *   - `displayName`      the user's full name
+     *   - `name.familyName`  the user's last name
+     *   - `name.givenName`   the user's first name
+     *   - `name.middleName`  the user's middle name
+     *   - `gender`           the user's gender: `male` or `female`
+     *   - `profileUrl`       the URL of the profile for the user on Facebook
+     *   - `emails`           the proxied or contact email address granted by the user
+     *
+     * @param {String} accessToken
+     * @param {Function} done
+     */
+    userProfile(accessToken, done) {
+        let url = uri.parse(this._profileURL);
 
-    this._oauth2.get(url, accessToken, (error, body, res) => {
-      if (error) return done(new InternalOAuthError('Failed to fetch user profile', error));
+        if (this._enableProof) {
+            // For further details, refer to https://developers.facebook.com/docs/reference/api/securing-graph-api/
+            let proof = crypto.createHmac('sha256', this._clientSecret).update(accessToken).digest('hex');
+            url.search = `${url.search ? url.search + '&' : ''}appsecret_proof=${encodeURIComponent(proof) }`;
+        }
 
-      try {
-        let json = JSON.parse(body);
-        let profile = {
-          provider: 'facebook',
-          id: json.id,
-          displayName: json.name || '',
-          name: {
-            familyName: json.last_name || '',
-            givenName: json.first_name || '',
-            middleName: json.middle_name || ''
-          },
-          gender: json.gender || '',
-          emails: [{
-            value: json.email || ''
-          }],
-          photos: [{
-            value: `https://graph.facebook.com/${json.id}/picture?type=large`
-          }],
-          _raw: body,
-          _json: json
+        if (this._profileFields) {
+            let fields = FacebookTokenStrategy.convertProfileFields(this._profileFields);
+            url.search = `${url.search ? url.search + '&' : ''}fields=${fields}`;
+        }
+
+        url = uri.format(url);
+
+        this._oauth2.get(url, accessToken, (error, body, res) => {
+            if (error) return done(new InternalOAuthError('Failed to fetch user profile', error));
+
+            try {
+                let json = JSON.parse(body);
+                let profile = {
+                    provider: 'facebook',
+                    id: json.id,
+                    displayName: json.name || '',
+                    name: {
+                        familyName: json.last_name || '',
+                        givenName: json.first_name || '',
+                        middleName: json.middle_name || ''
+                    },
+                    gender: json.gender || '',
+                    emails: [{
+                        value: json.email || ''
+                    }],
+                    photos: [{
+                        value: `https://graph.facebook.com/${json.id}/picture?type=large`
+                    }],
+                    _raw: body,
+                    _json: json
+                };
+
+                done(null, profile);
+            } catch (e) {
+                done(e);
+            }
+        });
+    }
+
+    /**
+     * Converts array of profile fields to string
+     * @param {Array} _profileFields Profile fields i.e. ['id', 'email']
+     * @returns {String}
+     */
+    static convertProfileFields(_profileFields) {
+        let profileFields = _profileFields || [];
+        let map = {
+            'id': 'id',
+            'displayName': 'name',
+            'name': ['last_name', 'first_name', 'middle_name'],
+            'gender': 'gender',
+            'profileUrl': 'link',
+            'emails': 'email',
+            'photos': 'picture'
         };
 
-        done(null, profile);
-      } catch (e) {
-        done(e);
-      }
-    });
-  }
-
-  /**
-   * Converts array of profile fields to string
-   * @param {Array} _profileFields Profile fields i.e. ['id', 'email']
-   * @returns {String}
-   */
-  static convertProfileFields(_profileFields) {
-    let profileFields = _profileFields || [];
-    let map = {
-      'id': 'id',
-      'displayName': 'name',
-      'name': ['last_name', 'first_name', 'middle_name'],
-      'gender': 'gender',
-      'profileUrl': 'link',
-      'emails': 'email',
-      'photos': 'picture'
-    };
-
-    return profileFields.reduce((acc, field) => acc.concat(map[field] || field), []).join(',');
-  }
-  
-  /**
-  * Parses an access token from request body, query, or headers using the accessTokenField parameter. When parsing the
-  * request header there are two possible strategies
-  *  - parse header object using a custom header specified by accessTokenField
-  *  - parse header object via OAuth2 RFC6750 bearer authorization
-  * 
-  * @param {Object} headers header object from a request
-  * @param {String} accessTokenField custom http field with access token directly set
-  * @param {String} authorizationField secondary http field that is RFC6750 compliant
-  * @returns {String} access token
-  */ 
-  static parseAccessToken(req, accessTokenField, authorizationField) {
+        return profileFields.reduce((acc, field) => acc.concat(map[field] || field), []).join(',');
+    }
     
-    if (req.body && req.body[accessTokenField]) {
-        return req.body[accessTokenField]
+    /**
+    * Parses an OAuth2 RFC6750 bearer authorization token, this method additionally is RFC 2616 compliant and respects
+    * case insensitive headers. 
+    * 
+    * @param {Object} req http request object
+    * @returns {String} value for field within body, query, or headers
+    */
+    parseOAuth2Token(req) {
+        const OAuth2AuthorizationField = 'Authorization';
+        return (
+            req.headers && (req.headers[OAuth2AuthorizationField] || req.headers[OAuth2AuthorizationField.toLowerCase()]) && (()=>{                
+                const bearerRE = /Bearer\ (.*)/;
+                let header = req.headers[OAuth2AuthorizationField] || req.headers[OAuth2AuthorizationField.toLowerCase()];
+                let match = bearerRE.exec(header);
+                return match[1];
+            })()
+        );        
     }
-    else if (req.query && req.query[accessTokenField]) {
-        return req.query[accessTokenField];
+    
+    /**
+    * Performs a lookup of the param field within the request, this method handles searhing the body, query, and header. 
+    * Additionally this method is RFC 2616 compliant and allows for case insensitive headers. This method additionally will
+    * delegate outwards to the OAuth2Token parser to validate whether a OAuth2 bearer token has been provided.     
+    * 
+    * @param {Object} req http request object
+    * @param {String} field 
+    * @returns {String} value for field within body, query, or headers
+    */
+    lookup(req, field) {
+        return (
+            req.body && req.body[field] ||
+            req.query && req.query[field] ||
+            req.headers && (req.headers[field] || req.headers[field.toLowerCase()]) ||
+            this.parseOAuth2Token(req)            
+            );
     }
-    // headers should be case insensitive, some libraries (like unirest) will lowercase all headers automatically
-    // lowercasing custom accessTokenField since users can override it        
-    else if ( req.headers && (req.headers[accessTokenField] || req.headers[accessTokenField.toLowerCase()]) ) {
-      return req.headers[accessTokenField];      
-    }    
-    else if ( req.headers && (req.headers[authorizationField] ||  req.headers[authorizationField.toLowerCase()]) ) {
-      let bearerRE = /Bearer\ (.*)/;
-      let header = req.headers[authorizationField] ||  req.headers[authorizationField.toLowerCase()];
-      let match = bearerRE.exec(header) ;
-      return match[1];
-    }
-  }
-  
+
+
+
 }
